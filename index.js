e92ba46d182ac55f5ad54cd235c152d7cae8646e
PostSupe <86279967+PostSupe@users.noreply.github.com>
2021-06-22 20:26:08 +1200

    Create index.js
    initial companion js

---
+++ index.js
@@ -0,0 +1,7 @@
+var tcp = require('../../tcp');
+var instance_skel = require('../../instance_skel');
+var debug;
+var log;
+
+// Blatently derived from https://github.com/bitfocus/companion-module-christie-projector/blob/master/index.js
+
